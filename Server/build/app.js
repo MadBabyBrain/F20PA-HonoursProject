b6dbe2d9b717afad66b6655db68ee5848d0fc6ea
MadBabyBrain <46905566+MadBabyBrain@users.noreply.github.com>
2023-02-22 05:20:44 +0000

    change get req

--- Server/build/app.js
+++ Server/build/app.js
@@ -4,7 +4,7 @@ exports.routes = void 0;
 const path_1 = require("path");
 var routes = (app) => {
     app.get('/:id', (req, res, next) => {
-        res.sendFile("req.params['id']", { root: (0, path_1.resolve)("C:/home/site/wwwroot/dist/honours-project") });
+        res.sendFile(req.params['id'], { root: (0, path_1.resolve)("C:/home/site/wwwroot/dist/honours-project") });
     });
 };
 exports.routes = routes;
--- Server/src/app.ts
+++ Server/src/app.ts
@@ -4,7 +4,7 @@ import { resolve } from "path";
 var routes = (app: Express) => {
 
     app.get('/:id', (req, res, next) => {
-        res.sendFile("req.params['id']", {root: resolve("C:/home/site/wwwroot/dist/honours-project") })
+        res.sendFile(req.params['id'], {root: resolve("C:/home/site/wwwroot/dist/honours-project") })
     })
 }
 
